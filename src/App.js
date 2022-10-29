ab19d9a2083ad50f0fc82a53595142c517e0bd2a
Sammydepoj <samueladepoju86@yahoo.com>
2022-10-29 19:20:08 +0100

    all files in one component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import './App.css';
-import Button from './components/Button';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Socials from './components/Socials';
+import userPic from '../src/components/assets/user.svg'
+import share from '../src/components/assets/sharebtn1.svg'
+import sharemobile from '../src/components/assets/sharebtn2.png'
+import slack from '../src/components/assets/slack.svg'
+import github from '../src/components/assets/Icon.svg'
+import I4G from '../src/components/assets/I4G.svg'
+import footerText from '../src/components/assets/Footer text.svg'
+import zuriLogo from '../src/components/assets/zurilogo.svg'
 
 function App() {
   // Slack username ='Sammydepoj'
   return (
     <div className="App">
-      <Header/>
 
-      {/* <input type={'text'} hidden id='slack' value={'Sammydepoj'} /> */}
-      <p id='slack'>Sammydepoj</p>
+      <div className="Header">
+        <img className='share' title='Share' src={share} alt='share button' />
+        <img className='share2' title='Share' src={sharemobile} alt=' share button' />
+
+        <img id='profile__img' src={userPic} alt='profile pic' />
+        <a className='userName' href='https://www.twitter.com/sammydepoj' title='link to my twitter handle' id='twitter'>Sammydepoj</a>
+    </div>
+
+      <p id='slack'>slack username: @Sammydepoj</p>
 
       <div className ="container">
-      <Button text='Twitter Link' page = 'https://www.twitter.com/sammydepoj' identity = 'twitter'/>
-      <Button text='Zuri books' page = 'https://books.zuri.team' identity = 'books'/>
-      <Button text='Zuri Team' page = 'https://training.zuri.team' identity = 'btn__zuri'/>
-      <Button text='Python Books' page = 'https://books.zuri.team/python-for-beginners?ref_id=sammydepoj' identity = 'book__python'/>
-      <Button text='Background Check for Coders' page = 'https://backgroundcheck.zuri.team' identity = 'pitch'/>
-      <Button text='Design Books' page = 'https://books.zuri.team/design-rules' identity = 'book__design'/>
+
+      <div className = 'link-container'>
+        <a className='links' href='https://www.twitter.com/sammydepoj' id='twitter' title='link to my twitter handle'>Twitter Link</a>
+      </div>
+      <div className = 'link-container'>
+        <a className='links' href='https://books.zuri.team' id='books' title='Zuri books'>Zuri books</a>
+      </div>
+      <div className = 'link-container'>
+        <a className='links' href='https://training.zuri.team' id='btn__zuri'title='Zuri team'>Zuri Team</a>
+      </div>
+      <div className = 'link-container'>
+        <a className='links' href='https://books.zuri.team/python-for-beginners?ref_id=sammydepoj' id='book__python' title='Python books'>Python Books</a>
+      </div>
+      <div className = 'link-container'>
+        <a className='links' href='https://backgroundcheck.zuri.team' id='pitch'>Background Check for Coders</a>
+      </div>
+      <div className = 'link-container'>
+        <a className='links' href='https://books.zuri.team/design-rules' id='book__design' title='Design books'>Design Books</a>
+      </div>
+     
       </div>
 
-      <Socials/>
-      <Footer/>
-      
-    </div>
+      <div className='icons'>
+        <a href='sammydepoj'id='slack-icon'><img src={slack} alt='slack-icon' /></a>
+
+        <a href='https://github.com/Sammydepoj'id='github'><img src={github} alt='github-icon'/></a>
+      </div>
+
+      <div className='footer-icons'>
+        <img src={zuriLogo} alt='zuri logo'/>
+        <img src={footerText} alt='hng-task' />
+        <img src ={I4G} alt='i4g logo' />
+      </div>
+      </div>
   );
 }
 
